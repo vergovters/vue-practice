7a6b5fd9167c52646af88de1708233b27c8fdf05
illia <fialkivskiji@gmail.com>
2024-05-07 15:45:41 +0300

    lifecycle

--- src/components/TestComponentTwo.vue
+++ src/components/TestComponentTwo.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-import { inject } from 'vue'
+import { inject, onBeforeMount, onMounted, onUnmounted, onUpdated } from 'vue'
 
     interface PageData {
         page: number;
     }
-
+    console.log("created")
     const page = inject<PageData>("page")
 
+    onBeforeMount(() => {
+        console.log("onBeforeMount")
+    })
+
+    onMounted(() => {
+        console.log("onMounted")
+    })
 
 </script>
 
